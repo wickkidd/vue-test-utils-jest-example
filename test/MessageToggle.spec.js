6b1062942483e67551286c644f770ec83c48dadf
Brian Kidd <bkidd6@bloomberg.net>
2018-06-18 19:48:58 -0400

    added a wrapper.setData() example

--- test/MessageToggle.spec.js
+++ test/MessageToggle.spec.js
@@ -3,18 +3,24 @@ import MessageToggle from '@/components/MessageToggle.vue'
 import Message from '@/components/Message.vue'
 
 describe('MessageToggle', () => {
+  const wrapper = shallowMount(MessageToggle)
+  const MessageComponent = wrapper.find(Message)
+
   it('toggles msg passed to Message when button is clicked', () => {
-    const wrapper = shallowMount(MessageToggle)
     const button = wrapper.find('#toggle-message')
 
     button.trigger('click')
-
-    const MessageComponent = wrapper.find(Message)
 
     expect(MessageComponent.props().msg).toEqual('message')
 
     button.trigger('click')
 
     expect(MessageComponent.props().msg).toEqual('toggled message')
+  })
+
+  it('passes msg to Message', () => {
+    wrapper.setData({ msg: 'foo' })
+
+    expect(MessageComponent.props().msg).toEqual('foo')
   })
 })
