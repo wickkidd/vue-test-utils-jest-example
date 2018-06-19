b77655866fbdb9246df948e7a0ef37f4e4c98a5c
Brian Kidd <bkidd6@bloomberg.net>
2018-06-19 12:48:12 -0400

    added AddItemButton to List

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,14 +1,21 @@
 <template>
-  <ul>
-    <li :key="item" v-for="item in items">
-      {{ item }}
-    </li>
-  </ul>
+  <div>
+    <ul>
+      <li :key="item" v-for="item in items">
+        {{ item }}
+      </li>
+    </ul>
+
+    <AddItemButton @addItem="$emit('addItem', $event)"></AddItemButton>
+  </div>
 </template>
 
 <script>
+  import AddItemButton from '@/components/AddItemButton.vue'
+
   export default {
     name: 'List',
+    components: {AddItemButton},
     props: [
       'items'
     ]
--- test/List.spec.js
+++ test/List.spec.js
@@ -3,20 +3,15 @@ import List from '@/components/List.vue'
 
 describe('List', () => {
   const items = ['item 1', 'item 2']
+  const wrapper = shallowMount(List, {
+    propsData: { items }
+  })
 
   it('renders li for each item in props.items', () => {
-    const wrapper = shallowMount(List, {
-      propsData: { items }
-    })
-
     expect(wrapper.findAll('li')).toHaveLength(items.length)
   })
 
   it('matches snapshot', () => {
-    const wrapper = shallowMount(List, {
-      propsData: { items }
-    })
-
     expect(wrapper.html()).toMatchSnapshot()
   })
 })
